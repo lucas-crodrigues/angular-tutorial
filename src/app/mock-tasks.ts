b9e7f8f3fef1b4681610b664e6813f17472ac129
lucascr <lcrodrigues1995@gmail.com>
2023-04-18 16:40:44 -0300

    Create mock-tasks.ts

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,16 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors appointment',
+    day: 'May 5yh at 14:30',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting',
+    day: 'May 6yh at 15:30',
+    reminder: false,
+  },  
+]
